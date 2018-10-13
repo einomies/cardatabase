a7be7b131c1622eca868866e3e1a3ed3cce52f18
einomies26 <einokamarainen@hotmail.com>
2018-10-13 18:26:43 +0300

    extends PagingAndSortingRepository

--- src/main/java/com/packt/cardatabase/domain/Car.java
+++ src/main/java/com/packt/cardatabase/domain/Car.java
@@ -11,11 +11,12 @@ public class Car {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
+
 	private String brand, model, color, registerNumber;
+	
 	private int year, price;
 
 	public Car() {
-
 	}
 
 	public Car(String brand, String model, String color, String registerNumber, int year, int price) {
--- src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -1,7 +1,62 @@
 package com.packt.cardatabase.domain;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
-public interface CarRepository extends CrudRepository<Car, Long> {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+//public interface CarRepository extends CrudRepository<Car, Long> {
+public interface CarRepository extends PagingAndSortingRepository<Car, Long> {
+
+	// Fetch cars by brand
+//	List<Car> findByBrand(String brand);
+
+	// Fetch cars by color
+	List<Car> findByColor(String color);
+
+	// Fetch cars by year
+	List<Car> findByYear(int year);
+
+	/*
+	 * There can be multiple fields after the By keyword, concatenated with the And
+	 * or Or keywords:
+	 */
+
+	// Fetch cars by brand and model
+	List<Car> findByBrandAndModel(String brand, String model);
+
+	// Fetch cars by brand or color
+	List<Car> findByBrandOrColor(String brand, String color);
+
+	/*
+	 * Queries can be sorted by using the OrderBy keyword in the query method:
+	 */
+
+	// Fetch cars by brand and sort by year
+	List<Car> findByBrandOrderByYearAsc(String brand);
+
+	/*
+	 * You can also create queries by using SQL statements, via the @Query
+	 * annotation. The following example shows the usage of a SQL query in
+	 * CrudRepository.
+	 */
+	// Fetch cars by brand using SQL
+	@Query("select c from Car c where c.brand = ?1")
+	List<Car> findByBrand(String brand);
+
+	// Fetch cars by brand (ending string) using SQL
+	@Query("select c from Car c where c.brand like %?1")
+	List<Car> findByBrandEndsWith(String brand);
+
+//	näillekin joku Query-annotaatio tms?
+
+	// Returns all entities sorted by the given options
+	Iterable<Car> findAll(Sort sort);
+
+	// Returns all entities according to given paging options
+	Page<Car> findAll(Pageable pageable);
 
 }
