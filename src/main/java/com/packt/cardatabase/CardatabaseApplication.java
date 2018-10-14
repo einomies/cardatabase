63aa9a2223fa523a5d2f757e4dc1abd5d7d8d03d
einomies26 <einokamarainen@hotmail.com>
2018-10-14 10:27:57 +0300

    saveCars01

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -8,9 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.packt.cardatabase.domain.Car;
 import com.packt.cardatabase.domain.CarRepository;
-import com.packt.cardatabase.init.CarUtils;
 
 @SpringBootApplication
 public class CardatabaseApplication {
@@ -22,8 +20,7 @@ public class CardatabaseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
-		logger.info("Spring Boot Application started");
-		System.out.println("muutos 01");
+		logger.info("Spring Boot CardatabaseApplication started");
 	}
 
 	@Bean
@@ -31,10 +28,6 @@ public class CardatabaseApplication {
 		return args -> {
 			// Save demo data to database
 			com.packt.cardatabase.init.CarUtils.saveCars01(repository);
-//			repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000));
-//			repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000));
-//			repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000));
-//			repository.save(new Car("Skoda", "Superb", "Purple", "RSV-932", 2011, 10000));
 		};
 	}
 
